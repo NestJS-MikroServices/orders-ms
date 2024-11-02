5e8ca066375d354fdcbcadfe964bb08b4cd723ce
Louis Rey <rlhardrock@gmail.com>
2024-11-01 23:40:19 -0500

    add folder config 'environment vars' & port 3002, npm i joi dotenv class-transform class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>
+  (
+    AppModule, {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    }
+  );
+
+  const looger = new Logger('ProductsMS-main');
+
+  //app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen();
+  looger.log("PRODUCTS-MICROSERVICE ACTIVATE");
 }
 bootstrap();
